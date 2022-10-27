dfa88cc9bd1d750efd26da8f3027fa01aaffbce0
lenovo <lenovo@10.48.77.106>
2022-10-27 11:37:47 +0300

    third

--- deneme2/src/deneme2/deneme2.java
+++ deneme2/src/deneme2/deneme2.java
@@ -3,8 +3,16 @@ package deneme2;
 public class deneme2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final int ARRAY_LENGHT =10;
+		int[] array = new int[ARRAY_LENGHT];
+		for(int counter = 0; counter<array.length; counter++) {
+			array[counter]= 2+2*counter;
+		}
+ System.out.printf("%s%8s%n", "Index","Value");
+ for(int counter=0; counter<array.length; counter++) {
+	 System.out.printf("%5d%8d%n", counter, array[counter]);
+	 System.out.println("");
+ }
 	}
 
 }
